4d963e21aacf96e1112367a27a9d192eab2b1888
Matthew Torma <Tormamatthew@gmail.com>
2024-11-22 17:08:56 -0600

    Extend register/login methods to User Model

--- prisma/index.js
+++ prisma/index.js
@@ -1,3 +1,29 @@
+const bcrypt = require("bcrypt");
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  model: {
+    user: {
+      async register(username, password) {
+        const hash = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({
+          data: username,
+          password: hash,
+        });
+        return user;
+      },
+      async login(username, password) {
+        try {
+          const customer = await prisma.user.findUniqueOrThrow({
+            where: { username },
+          });
+          const valid = await bcrypt.compare(password, user.password);
+          if (!valid) throw Error(`Woah back up, BUddy.`);
+          return customer;
+        } catch (e) {
+          next(e);
+        }
+      },
+    },
+  },
+});
 module.exports = prisma;
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 app = express();
 PORT = 3000;
 
-app.use(express.json());
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalURL}`);
 });
+app.use(express.json());
 
-app.use(require("./api/auth").router);
-app.use("/orders", require("./api/orders"));
-app.use("/products", require("./api/products"));
-app.use("/users", require("./api/users"));
+// app.use(require("./api/auth").router);
+// app.use("/orders", require("./api/orders"));
+// app.use("/products", require("./api/products"));
+// app.use("/users", require("./api/users"));
 
 app.use((req, res, next) => {
   next({ status: 404, message: `Endpoint not Found, Magellan` });
