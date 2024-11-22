b4f805c961d5b87e24fe2e6626b5fb31e80c54ee
Matthew Torma <Tormamatthew@gmail.com>
2024-11-22 12:34:09 -0600

    Add Routes to Server

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalURL}`);
 });
 
+app.use(require("./api/auth").router);
+app.use("/orders", require("./api/orders"));
+app.use("/products", require("./api/products"));
+app.use("/users", require("./api/users"));
+
 app.use((req, res, next) => {
   next({ status: 404, message: `Endpoint not Found, Magellan` });
 });
