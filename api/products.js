e632873970017b0219ade3a7ec64230a1d503481
Matthew Torma <Tormamatthew@gmail.com>
2024-11-25 00:16:49 -0600

    Add Product Route

---
+++ api/products.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+module.exports = router;
+
+const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.json(products);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await prisma.product.findUniqueOrThrow({
+      where: { id: +id },
+    });
+    res.json(product);
+  } catch (e) {
+    next(e);
+  }
+});
